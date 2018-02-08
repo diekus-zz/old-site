1d1dd3aa5b002932ae353e9ae1ec0ffb44bb5e32
Diego <diekus@gmail.com>
2018-02-08 12:35:09 +0000

    adds auto night mode

--- js/diekus.js
+++ js/diekus.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-});
+var style = document.createElement('link');
+
+var date1 = new Date(Date.now());
+var h = date1.getHours();
+
+if(h > 7 && h < 20){
+    style.setAttribute('href', 'css/style.css');
+}
+else{
+    style.setAttribute('href', 'css/style-night.css');
+}
+
+style.setAttribute('rel', 'stylesheet');
+document.querySelector('head').appendChild(style);
